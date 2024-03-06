d198daccb6de343ed8b763c71d50f839361f5f37
Mr. Javascript <agnel1692@yahoo.com>
2024-03-06 13:20:13 +0400

    revert changes

--- app/api/auth/update-password/route.ts
+++ app/api/auth/update-password/route.ts
@@ -7,11 +7,9 @@ export async function POST(request: NextRequest) {
     const body: { password: string; nonce: string } = await request.json();
     const supabase = createRouteHandlerClient({ cookies });
 
-    const { data, error } = await supabase.auth.updateUser({
+    const { error } = await supabase.auth.updateUser({
       password: body.password,
     });
-
-    console.log('*****data', data, error, body.nonce);
 
     if (error) {
       return new Response(JSON.stringify({ data: null, error: true }), {
--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from 'next';
-import { ReadonlyURLSearchParams } from 'next/navigation';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
 
 import UpdatePassword from 'components/update-password/update-password';
 
@@ -10,28 +7,11 @@ export const metadata: Metadata = {
   description: 'Quotation and Invoice Management Software',
 };
 
-export default async function UpdatePasswordPage({
-  searchParams,
-}: {
-  searchParams: ReadonlyURLSearchParams;
-}) {
+export default async function UpdatePasswordPage() {
   const year = new Date().getFullYear();
 
-  async function create() {
-    'use server';
-
-    console.log('********hahahahah');
-
-    if (searchParams?.code) {
-      const supabase = createRouteHandlerClient({ cookies });
-      await supabase.auth.exchangeCodeForSession(searchParams?.code);
-
-      console.log('********hahahahah');
-    }
-  }
-
   return (
-    <main onLoad={create}>
+    <main>
       <div className="container h-full flex flex-col justify-center items-center">
         <UpdatePassword />
 
